254fec2e84187ba2dbb06b1c9f006ddcc8a0310d
Caspar Schroeder <ccs.schroeder@gmail.com>
2018-08-17 17:09:48 +0200

    Initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :cocktails do
     resources :doses, only: [:create]
   end
 
-  root to: "cocktails#index"
+  root to: 'cocktails#index'
 end
